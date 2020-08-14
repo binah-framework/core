001725c74f89686619332f92b1223c42650f5078
Ranjit Jhala <jhala@cs.ucsd.edu>
2020-08-14 13:39:34 -0700

    done select

--- src/Binah.hs
+++ src/Binah.hs
@@ -3,10 +3,12 @@
 
 module Binah where
 
+import           ProofCombinators
 import qualified Data.Set as S
 import           Labels 
 import           LIO
-import           ProofCombinators
+import           LIOCombinators
+
 -------------------------------------------------------------------------------
 -- | Basic DB values 
 -------------------------------------------------------------------------------
@@ -18,9 +20,8 @@ type Val = Int
 {-@ type LValL L = {v: Labeled Val | lvLabel v == L } @-}
 
 -------------------------------------------------------------------------------
-type Policy = Val -> Val -> Label
 
-type Field  = Row -> Labeled Val
+type Policy = Val -> Val -> Label
 
 -------------------------------------------------------------------------------
 -- | DB Rows
@@ -43,15 +44,6 @@ rVal1 r = lvValue (rFld1 r)
 rVal2 :: Row -> Val
 rVal2 r = lvValue (rFld2 r)
 
-
--- {-@ measure rV1 @-}
--- rV1 :: Row -> Val
--- rV1 (Row v _ _ _) = v 
-
--- {-@ measure rV2 @-}
--- rV2 :: Row -> Val
--- rV2 (Row _ v _ _) = v 
-
 -------------------------------------------------------------------------------
 -- | Policy-indexed Row -------------------------------------------------------
 -------------------------------------------------------------------------------
@@ -59,17 +51,12 @@ rVal2 r = lvValue (rFld2 r)
 {-@ type RowP P1 P2 = {r: Row | okLabel P1 rFld1 r && okLabel P2 rFld2 r} @-}
 
 {-@ reflect okLabel @-}
-okLabel :: Policy -> Field -> Row -> Bool
+okLabel :: Policy -> (Row -> Labeled Val) -> Row -> Bool
 okLabel p fld r = lvLabel (fld r) == policyLabel p r
 
 {-@ reflect policyLabel @-}
 policyLabel :: Policy -> Row -> Label
 policyLabel p r = p (rVal1 r) (rVal2 r)
-
--- TODO: Does this need to be an LIO?
-{-@ mkRow :: p1:_ -> p2:_ -> v1:_ -> v2:_ -> RowP p1 p2 @-}
-mkRow :: Policy -> Policy -> Val -> Val -> Row
-mkRow p1 p2 v1 v2 = Row (Labeled (p1 v1 v2) v1) (Labeled (p2 v1 v2) v2)
 
 {-@ reflect approx @-}
 approx :: Policy -> Row -> Label -> Bool
@@ -98,12 +85,6 @@ ttPol2 (Table _ p2 _) = p2
 {-@ type TableP P1 P2 = {t:Table | ttPol1 t == P1 && ttPol2 t = P2} @-}
 
 -------------------------------------------------------------------------------
--- | "Proofs" that a Policy is bounded by a Label -----------------------------
--------------------------------------------------------------------------------
-{-@ type SubPL P L = (sv1:Val -> sv2:Val -> {pf:() | leq (P sv1 sv2) L}) @-}
-type SubPL = Val -> Val -> () 
-
--------------------------------------------------------------------------------
 -- | Field Projection ---------------------------------------------------------
 -------------------------------------------------------------------------------
 
@@ -127,6 +108,18 @@ data VOp  = Eq | Le | Ne | Ge
 data BOp  = And | Or
 data Fld  = F1 | F2
 data Pred = Atom VOp Fld Val | BOp BOp Pred Pred 
+
+{-@ reflect vOp @-}
+vOp :: VOp -> Val -> Val -> Bool
+vOp Eq v1 v2 = v1 == v2 
+vOp Le v1 v2 = v1 <= v2 
+vOp Ne v1 v2 = v1 /= v2 
+vOp Ge v1 v2 = v1 >= v2 
+
+{-@ reflect bOp @-}
+bOp :: BOp -> Bool -> Bool -> Bool
+bOp And b1 b2 = b1 && b2
+bOp Or  b1 b2 = b1 || b2
 -------------------------------------------------------------------------------
 
 {-@ reflect fldLabel @-}
@@ -151,7 +144,7 @@ predLabel (BOp  _ f g) p1 p2 x1 x2 = (predLabel f p1 p2 x1 x2) `S.intersection` 
 -- TODO the `S.intersection` instead of plain `join` is due to https://github.com/ucsd-progsys/liquidhaskell/issues/1738
 
 -------------------------------------------------------------------------------------------
--- | Evaluating a Predicate on a Row
+-- | Denotation of Predicates
 -------------------------------------------------------------------------------------------
 {-@ reflect interpFld @-}
 interpFld :: Fld -> Val -> Val -> Val
@@ -163,6 +156,13 @@ interpPred :: Pred -> Val -> Val -> Bool
 interpPred (Atom o fld val) v1 v2 = vOp o (interpFld  fld v1 v2) val
 interpPred (BOp  o f   g)   v1 v2 = bOp o (interpPred f   v1 v2) (interpPred g v1 v2)
 
+{-@ reflect interpPredR @-}
+interpPredR :: Pred -> Row -> Bool
+interpPredR pred r = interpPred pred (rVal1 r) (rVal2 r)
+
+-------------------------------------------------------------------------------------------
+-- | Evaluating a Predicate on a Row
+-------------------------------------------------------------------------------------------
 {-@ evalPred :: p1:_ -> p2:_ -> l:_ -> 
                 pred:_ -> 
                 r:{RowP p1 p2 | approx (predLabel pred p1 p2) r l} -> 
@@ -180,21 +180,16 @@ evalPred p1 p2 l (BOp  o f g) r =
     (evalPred p1 p2 l f r)
     (evalPred p1 p2 l g r)
 
-{-@ reflect vOp @-}
-vOp :: VOp -> Val -> Val -> Bool
-vOp Eq v1 v2 = v1 == v2 
-vOp Le v1 v2 = v1 <= v2 
-vOp Ne v1 v2 = v1 /= v2 
-vOp Ge v1 v2 = v1 >= v2 
 
-{-@ reflect bOp @-}
-bOp :: BOp -> Bool -> Bool -> Bool
-bOp And b1 b2 = b1 && b2
-bOp Or  b1 b2 = b1 || b2
+-------------------------------------------------------------------------------------------
+-- | "Proofs" that a Policy `p` is approximated by a Label `l` 
+--   specify constraints of the form `forall v1, v2. (policy v1 v2) `leq` l 
+-------------------------------------------------------------------------------------------
+type SubPL = Val -> Val -> () 
 
------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 -- | Vanilla Select from a Table
------------------------------------------------------------------------------
+-------------------------------------------------------------------------------------------
 
 {-@ select :: p1:_ -> p2:_ -> l:_ -> pred:_ 
            -> (sv1:_ -> sv2:_ -> { leq (predLabel pred p1 p2 sv1 sv2) l }) 
@@ -214,7 +209,7 @@ select p1 p2 l pred pf (Table _ _ rows) =
   @-}
 
 -- TODO: the above signature FAILS with `interpPredR pred r` ==> `interpPred pred (rVal1 r) (rVal2 r)` 
---       maybe some strange ETA-expansion bug in PLE?
+--       likely due to some strange ETA-expansion bug in PLE?
 
 select' :: Policy -> Policy -> Label -> Pred -> SubPL -> Table -> LIO [Row]
 select' p1 p2 l pred pf (Table _ _ rows) = 
@@ -224,46 +219,11 @@ select' p1 p2 l pred pf (Table _ _ rows) =
     (\r -> evalPred p1 p2 (if cond r then l else S.empty) pred (r ? pf (rVal1 r) (rVal2 r))) 
     rows
 
-{-@ reflect interpPredR @-}
-interpPredR :: Pred -> Row -> Bool
-interpPredR pred r = interpPred pred (rVal1 r) (rVal2 r)
+------------------------------------------------------------------------------------------------------
+-- TODO: insert, update ... 
+------------------------------------------------------------------------------------------------------
 
-{-
-
-A && B 
-
-if A then B else EMPTY
-
-filterM :: (f: a -> Bool) -> (x:a -> TIO {b:Bool | b = f x} { } S.empty)
--- filterM :: ∀α,i,f .(x : α → TI {Bool | ν ⇒ f x} ⟨f x ∧ i⟩) → [α] → TI [{α | f ν}] ⟨i⟩
--}
------------------------------------------------------------------------------
-{- JUNK 
--------------------------------------------------------------------------------
--- fieldLabel :: Row -> Field -> Label
--- fieldLabel r F1 = lvLabel (rFld1 r)
--- fieldLabel r F2 = lvLabel (rFld2 r)
--- predLabel :: Row -> Pred -> Label
--- predLabel r (Atom _ f _) = fieldLabel r f
--- predLabel r (And p1 p2)  = (predLabel r p1) `join` (predLabel r p2)
-
-   proj1 :: r:Row P1 P2 -> LIO Val (P1 (rVal1 r) (rVal2 r)) Bot
-
-   Pred_1
-   Pred_2
-   Pred_1_2
-   
-   eval_1   :: (forall v1 v2. P1 v1 v2 <: l) => Pred_1 -> Row P1 P2 -> LIO Bool l Bot 
-   eval_2   :: (forall v1 v2. P2 v1 v2 <: l) => Pred_1 -> Row P1 P2 -> LIO Bool l Bot 
-   eval_1_2 :: (forall v1 v2. P1 v1 v2 \leq P2 v1 v2  <: l) => Pred_1 -> Row P1 P2 -> LIO Bool l Bot 
-
-
-   eval_1   :: Pred_1 -> r:Row P1 P2 -> LIO Bool (rLabel1 r) Bot 
-   eval_2   :: Pred_2 -> r:Row P1 P2 -> LIO Bool (rLabel2 r) Bot 
-   eval_1_2 :: Pred_2 -> r:Row P1 P2 -> LIO Bool (rLabel1 r `join` rLabel2 r) Bot 
-
-   eval     :: (forall r. funky r p `leq` l) => p:Pred -> r:Row P1 P2 -> LIO Bool l Bot 
-   eval     :: p:Pred -> r:Row P1 P2 -> LIO Bool (funky r p) Bot 
-
-
- -}
+-- TODO: Does this need to be an LIO?
+{-@ mkRow :: p1:_ -> p2:_ -> v1:_ -> v2:_ -> RowP p1 p2 @-}
+mkRow :: Policy -> Policy -> Val -> Val -> Row
+mkRow p1 p2 v1 v2 = Row (Labeled (p1 v1 v2) v1) (Labeled (p2 v1 v2) v2)
---
+++ src/Combinators.hs
@@ -0,0 +1,77 @@
+-------------------------------------------------------------------------------
+-- | LIO Combinators ----------------------------------------------------------
+-------------------------------------------------------------------------------
+
+{-@ LIQUID "--reflection" @-}
+
+module LIOCombinators where
+
+import qualified Data.Set as S
+import                       Labels
+import                       LIO
+
+-------------------------------------------------------------------------------
+{-@ lmap :: l:_ -> l':_ -> (a -> b) -> TIO a l l' -> TIO b l l' @-}
+-------------------------------------------------------------------------------
+lmap :: Label -> Label -> (a -> b) -> LIO a -> LIO b
+lmap l l' f act = bind l l' l S.empty act (\x -> ret l (f x))
+
+-------------------------------------------------------------------------------
+{-@ lmap2 :: l:_ -> l':{leq l l'} -> 
+             (a -> b -> c) -> TIO a l l' -> TIO b l l' -> TIO c l l' 
+  @-}
+-------------------------------------------------------------------------------
+lmap2 :: Label -> Label -> (a -> b -> c) -> LIO a -> LIO b -> LIO c
+lmap2 l l' f act1 act2 = 
+  bind l l' l l' act1 (\x ->
+    bind l l' l S.empty act2 (\y -> 
+      ret l (f x y)
+    )
+  )
+
+-------------------------------------------------------------------------------
+{-@ filterM :: l:_ -> l':{leq l l'} -> 
+               (a -> TIO Bool l l') -> [a] -> TIO [a] l l' 
+  @-}
+-------------------------------------------------------------------------------
+filterM :: Label -> Label -> (a -> LIO Bool) -> [a] -> LIO [a] 
+filterM l _  _ []     = ret l []
+filterM l l' p (x:xs) = bind l l' l l' (p x) (\b -> 
+                          bind l l' l l' (filterM l l' p xs) (\ys -> 
+                            ret l (if b then x:ys else ys) 
+                          )
+                        )
+
+-------------------------------------------------------------------------------
+{-@ filterM' :: any:_ -> i:_ -> o:{leq i o} -> cond:(a -> Bool) -> 
+                (x:_ -> TIO {b:Bool | b => cond x} {if (cond x) then i else any} o) -> 
+                [a] ->
+                TIO [{v:a | cond v}] i o 
+  @-}
+-------------------------------------------------------------------------------
+filterM' :: Label -> Label -> Label -> (a -> Bool) -> (a -> LIO Bool) -> [a] -> LIO [a]
+filterM' _ i _ _    p [] = 
+  ret i []
+filterM' any i o cond p (x:xs) = 
+  bind i o i o (downgrade (cond x) i o (p x)) (\b -> 
+    bind i o i o (filterM' any i o cond p xs) (\ys -> 
+      ret i (if b then x:ys else ys) 
+    )
+  ) 
+
+-------------------------------------------------------------------------------
+{-@ filterM'' :: i:_ -> o:{leq i o} -> cond:(a -> Bool) -> 
+                 (x:_ -> TIO {b:Bool | b => cond x} {if (cond x) then i else S.empty} o) -> 
+                 [a] ->
+                 TIO [{v:a | cond v}] i o 
+  @-}
+-------------------------------------------------------------------------------
+filterM'' :: Label -> Label -> (a -> Bool) -> (a -> LIO Bool) -> [a] -> LIO [a]
+filterM'' i _ _    p [] = 
+  ret i []
+filterM'' i o cond p (x:xs) = 
+  bind i o i o (downgrade (cond x) i o (p x)) (\b -> 
+    bind i o i o (filterM'' i o cond p xs) (\ys -> 
+      ret i (if b then x:ys else ys) 
+    )
+  ) 
